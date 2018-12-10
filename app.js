5b16f49fb0b17e4d21f508d3369673d2a5127c40
HaqqiAhmad11 <ahmadhaqqi11@gmail.com>
2018-12-10 17:41:16 +0700

    Second Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+//npm init
+//npm install express --s
+
+//inisialisasi express
+const express = require("express")
+const app = express()
+
+var statuslampu = 0;
+var statuslampu2 = 0;
+
+app.listen(3000, function(){
+    console.log("Server is running")
+})
+
+app.get("/:lampu",function(req,res){
+    statuslampu = req.params.lampu
+    res.redirect("/")
+})
+
+app.get("/",function(req,res){
+    res.send({
+        lampu:statuslampu
+    })
+})
